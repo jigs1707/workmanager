48eced724e599e172522815c53d88deda9ed71eb
jigs1707 <79247171+jigs1707@users.noreply.github.com>
2021-03-12 13:44:56 +1100

    test3

--- src/workmanager/HomepageController.java
+++ src/workmanager/HomepageController.java
@@ -5,23 +5,65 @@
  */
 package workmanager;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-/**
- * FXML Controller class
- *
- * @author jigssharma
- */
-public class HomepageController implements Initializable {
+public class HomepageController {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    @FXML
+    private MenuItem exit;
+
+    @FXML
+    private Button profile;
+
+    @FXML
+    private Button availabilities;
+
+    @FXML
+    private Button roster;
+
+    @FXML
+    private Button staff;
+
+    @FXML
+    private Button status;
+
+    @FXML
+    private Button signOut;
     
+    
+    public void onSignOut()
+    {
+        try {
+            FXMLLoader loader = new FXMLLoader(WorkManager.class.getResource("LoginScreen.fxml"));
+            AnchorPane pane = loader.load();
+            
+            Scene scene = new Scene(pane);
+            
+            
+            Stage primaryStage = new Stage();
+            
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Login");
+            primaryStage.setResizable(false);
+            primaryStage.show();
+            
+            Stage stage = (Stage) signOut.getScene().getWindow();
+            stage.close();
+        } 
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
+
+
